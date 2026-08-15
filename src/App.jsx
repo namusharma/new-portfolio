1f8567e979e02846f51c9c3a404ea424cd573b68
namusharma <namya45840@gmail.com>
2026-08-15 22:23:20 +0530

    chnages in many files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './Sections/Hero'
+
+const App = () => {
+  return (
+   <>
+   <Hero/>
+   </>
+  )
+}
+
+export default App
---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+  <section id="hero" className ="relative overflow-hidden"> 
+  <div>
+    
+  </div>
+  
+  </section>
+  )
+}
+
+export default Hero
