056215c9648a5503811fbe3e6afe900ec2025e1d
namusharma <namya45840@gmail.com>
2026-08-30 17:22:25 +0530

    some changes

--- src/Sections/ShowcaseSection.jsx
+++ src/Sections/ShowcaseSection.jsx
@@ -8,16 +8,26 @@ const ShowcaseSection = () => {
 {/* LEFT */}
 <div className ="first-project-wrapper">
     <div className="image-wrapper">
-         <img src="/images/project1.png" alt="Ryde"/>
+         <img src="/images/project1.png" alt="StackShow"/>
 
     </div>
     <div className="text-content">
-
+      
+  <h2> An interactive platform where developers can display their tech stack, skills, and projects. </h2>
+  <p className ="text-white-50 md:text-xl">
+    Built with React Native and Tailwind CSS to deliver a fast, responsive, and user-friendly experience.
+ </p>
     </div>
 
 </div>
 
 {/* RIGHT */}
+<div className ="project-list-wrapper overflow-hidden">
+  <div className="project">
+<img src="/images/project2.png" alt="Project 2"/>
+
+  </div>
+</div>
 
     </div>
 
